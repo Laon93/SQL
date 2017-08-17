808b4b30ca9b33db0cd75f88335c49526ddce0d2
YeonJin <duswls0515@gmail.com>
2017-08-17 09:09:34 +0900

    17.08.17 09:09

--- Worksheet 1.sql
+++ Worksheet 1.sql
@@ -10,17 +10,3 @@ select first_name || ' ' || last_name as "이름", salary, (salary + salary * nv
 
 select nvl(manager_id,0) from employees;
 
-
-
-
---1. 전체직원을 입사일 기준 오름차순으로 정렬하되, 이름, 이메일 전화번호 순서로 출력
---단, 이름 이메일 전화번호로 컬럼이름을 대체할 것
-select last_name 이름, email 이메일, phone_number 전화번호 from employees order by hire_date asc;
-
---2. 업무별로 업무이름과 최고임금을 내림차순으로 정렬 (DESC)
-select JOB_TITLE , MAX_SALARY from jobs order by Job_title,max_salary desc;
-
---3. 매니저가 없는 직원은 몇명?
-select * from employees;
-select count (last_name) from employees where manager_id=null;
-
