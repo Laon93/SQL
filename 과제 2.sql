3bd208ab17415d2514be8e1ec28ebbdd9652fd56
YeonJin <duswls0515@gmail.com>
2017-08-17 17:12:41 +0900

    17.08.17 17:12

---
+++ 과제 2.sql
@@ -0,0 +1,51 @@
+-- 1. 직원중에 최고임금과 최저임금을 "최고임금", "최저임금" 프로젝션 타이틀로 함께 출력
+-- 	    최고임금 - 최저임금 이란 타이틀로 출력
+select * from employees;
+
+select max(salary)  최고임금, min(salary)  최저임금,
+ max(salary)-min(salary) "최고임금 - 최저임금" from employees;
+ 
+ 
+-- 2. 마지막으로 신입사원이 들어온 날은?
+select * from employees;
+select max(hire_date) from employees;
+
+-- 3. 부서별로 평균임금, 최고임금, 최저임금을 부서(department_id)와 함께 출력하고 정렬순서는
+--    평균연봉, 최고임금, 최저임금의 내림차순 (Group by 사용)
+select * from employees;
+select department_name, salary from employees;
+select department_id, avg(salary) 평균, max(salary) 최고, min(salary) "최소"
+FROM employees where department_id is not null 
+group by department_id 
+order by avg(salary) desc, max(salary) desc, min(salary) desc;
+
+-- 4. 업무별로 평균임금, 최고임금 최저임금을 업무와 함께 출력(Group by 사용)
+select * from employees;
+select job_id, salary from employees;
+select job_id, avg(salary) 평균, max(salary) 최고, min(salary) "최소"
+FROM employees where manager_id is not null 
+group by job_id
+order by avg(salary) desc, max(salary) desc, min(salary) desc;;
+
+-- 5. 가장 오래 근속한 직원의 입사일은 언제? --년 --월 --일로 출력
+select min(to_char(hire_date, 'yyyy"년" mm"월" dd"일"')) 입사일
+from employees;
+
+-- 6. 평균 임금과 최저 입금의 차이가 2000 미만인 부서,
+--    평균임금, 최저임금, (평균임금 - 최저임금)--> order by 조건
+select * from employees;
+select department_id from employees;
+select department_id, max(salary), min(salary),
+avg(salary) - min(salary) 
+from employees  where department_id is not null
+group by department_id having avg(salary) - min(salary) < 2000
+order by avg(salary) - min(salary) desc;
+
+
+-- 7. 업무별로 최고임금과 최저임금의 차이를 출력
+select * from employees;
+select job_id, max(salary) - min(salary) 
+from employees 
+group by job_id
+order by max(salary) - min(salary) desc;
+
---
+++ 과제3.sql
@@ -0,0 +1,89 @@
+--1. 각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 
+--   부서명(department_name), 매니저(manager)의 이름(first_name)을 조회
+select * from employees;
+select * from departments;
+
+select employee_id, first_name from employees;
+--manager 이름 출력
+select b.department_name, a.first_name
+from employees a, departments b
+where a.employee_id = b.manager_id;
+
+select a.employee_id, a.first_name, 
+	c.department_name, b.first_name manager_name
+from employees a, employees b, departments c
+where a.department_id = c.department_id
+and a.manager_id = b.employee_id
+order by a.employee_id; 
+
+
+--2. 지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로 출력
+select * from regions;
+select * from countries;
+
+select regions.region_name, countries.country_name
+from regions, countries
+where regions.region_id = countries.region_id
+order by countries.COUNTRY_NAME desc;
+
+--3. 각 부서(department)에 대해서 부서번호(department_id), 이름(department_name), 
+-- 매니저(manager)의 이름(first_name), 위치(locations)한 도시(city), 
+-- 나라(countries)의 이름(countries_name) 그리고 지역구분(regions)의 이름(resion_name)까지 
+-- 전부 출력해 
+
+select * from departments;
+select * from employees;
+select * from countries;
+select * from regions;
+select * from locations;
+
+select d.department_id, d.department_name, e.first_name, 
+l.city , c.country_name, r.region_name
+from departments d, employees e, countries c, regions r, locations l
+where d.manager_id = e.employee_id;
+
+--4. ‘Public Accountant’의 직책(job_title)으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력
+-- (현재 ‘Public Accountant’의 직책(job_title)으로 근무하는 사원은 고려하지 않습니다.) 
+-- 이름은 first_name과 last_name을 합쳐 출력합니다. 
+
+select * from job_history;
+select * from jobs;
+select * from employees;
+
+select e.first_name || ' ' || e.last_name
+from job_history jh, jobs j, employees e
+where j.job_title = 'Public Accountant'
+and jh.employee_id = e.employee_id;
+
+--5. 같은 성(last_name)을 가진 직원들의 사번(employee_id), 이름(firt_name), 
+-- 성(last_name)과 부서 이름을 조회하여 성(last_name)순서로 정렬
+select count(*) from employees;
+select * from employees
+where employees.last_name = 'King';
+select * from departments;
+
+select distinct a.employee_id, a.first_name, a.last_name, c.department_name
+from employees a, employees b, departments c
+where a.last_name = b.last_name
+and a.employee_id != b.employee_id
+and a.department_id = c.department_id
+order by a.last_name desc;
+
+-- 6.자신의 매니저보다 채용일(hire_date)이 빠른 사원의 사번(employee_id), 
+--  성(last_name)과 채용일(hire_date)을 조회하세요 
+select * from employees;
+
+select a.employee_id, b.employee_id manager_id , a.hire_date, b.hire_date
+from employees a, employees b
+where a.manager_id = b.employee_id
+and a.hire_date < b.hire_date;
+--and to_date(a.hire_date) < to_date( b.hire_date);
+
+
+
+
+
+
+
+
+
